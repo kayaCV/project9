bee936cd007a363b74fc510fc1afa5dc75137993
Carla <carlavdebarros@gmail.com>
2020-01-01 12:07:03 -0500

    fix message for successful connection to database

--- app.js
+++ app.js
@@ -4,10 +4,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
-
+const {sequelize} = require('./db');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+
+
+(async  () => {
+  try {
+      // TO TEST CONNECTION              1st test ///
+      await sequelize.authenticate();      
+      console.log('Connection to the database successful!');
+   
+  } catch (error) {
+      console.error('Error connecting to the database: ', error);
+  }
+})();
+
 
 // create the Express app
 const app = express();
